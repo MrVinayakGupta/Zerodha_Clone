f39b52de111bfeee7619ec2b815d208fe3f51888
Mr Vinayak Gupta <163561684+MrVinayakGupta@users.noreply.github.com>
2026-07-25 21:50:01 +0530

    Create .env

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
+require('dotenv').config()
+
 const express = require("express");
+const mongoose = require("mongoose")
+const passport = require("passport-local-mongoose")
+
+const port = process.env.PORT || 3002;
+const url = process.env.MONGO_URL;
 
 const app = express();
 
-app.listen(3001, ()=>{
+
+app.listen(port, ()=>{
   console.log("Server is listen on port 3001");  
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error("DB connection error:", err));
+
 })
